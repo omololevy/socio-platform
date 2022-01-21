5cc34a74f3f0696ab777c2dc812a26657b0940be
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2022-01-21 08:06:18 +0300

    Created loginand register component

--- Alumni/src/app/app.module.ts
+++ Alumni/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,

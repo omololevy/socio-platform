96bab8a742496cbf4857afe4516d4c0161c45e2b
Levy Omolo <levy.omolo@student.moringaschool.com>
2022-01-21 16:27:17 +0300

    Add the login and register components route paths in the app-routing module

--- Alumni/src/app/app-routing.module.ts
+++ Alumni/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
 
 import { HomePageComponent } from './components/home-page/home-page/home-page.component';
 import { BlogsComponent } from './components/blogs/blogs/blogs.component';
@@ -8,6 +10,12 @@ const routes: Routes = [
   { path: 'home', component: HomePageComponent },
   { path: 'blogs', component: BlogsComponent },
   
+
+ 
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '', redirectTo: 'index', pathMatch: 'full'},
+  {path: '**', redirectTo: 'index', pathMatch: 'full'}
 ];
 
 @NgModule({

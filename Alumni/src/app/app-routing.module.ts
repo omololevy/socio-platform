b1bc079487a0375626dfa079358df17b1c7afb8c
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2022-01-21 11:18:03 +0300

    Added login and register paths

--- Alumni/src/app/app-routing.module.ts
+++ Alumni/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ 
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '', redirectTo: 'index', pathMatch: 'full'},
+  {path: '**', redirectTo: 'index', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

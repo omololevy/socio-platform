4c006a0ebb606b11d27ca4af1c9c7c972c8c9b89
Nur tusa <nur.tusa@student.moringaschool.com>
2022-01-21 18:51:51 +0300

    adding about-us page

--- Alumni/src/app/app.module.ts
+++ Alumni/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,

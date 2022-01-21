d50678a820bf6eb885e689909427b6c611821fe8
Levy Omolo <levy.omolo@student.moringaschool.com>
2022-01-21 15:48:54 +0300

    Add the routes paths in a list form and import the home and blogs components

--- Alumni/src/app/app-routing.module.ts
+++ Alumni/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePageComponent } from './components/home-page/home-page/home-page.component';
+import { BlogsComponent } from './components/blogs/blogs/blogs.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'blogs', component: BlogsComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+
+
 })
 export class AppRoutingModule { }

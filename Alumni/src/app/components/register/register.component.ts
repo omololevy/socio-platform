aaebb4e68297769c19227536abcf44dd94c91b2b
Levy Omolo <levy.omolo@student.moringaschool.com>
2022-01-24 18:56:45 +0300

    Adjust the componet typescript file for the register component for api consumption

--- Alumni/src/app/components/register/register.component.ts
+++ Alumni/src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthapiService } from 'src/app/services/authapi.service';
+
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  passwordMismatch: boolean = false;
+  usernameExists: boolean = false;
 
-  constructor() { }
+  constructor(private service: AuthapiService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+  registerUser(form: NgForm) {
+    this.passwordMismatch = false
+    this.usernameExists = false
+    const email = form.value.email;
+    const password = form.value.password;
+    const confirmPassword = form.value.password2;
+    const first_name = form.value.first_name;
+    const last_name = form.value.last_name;
+    const username = form.value.username;
+    if (password == confirmPassword) {
+      this.service
+        .registerUser(username, password, email, first_name, last_name)
+        .subscribe(
+          (response) => {
+            console.log(response);
+            this.router.navigateByUrl('/login');
+          },
+          (error) => {
+            this.usernameExists = true
+            console.log(error);
+          }
+        );
+    }
+    else {
+      this.passwordMismatch = true
+    }
   }
 
 }

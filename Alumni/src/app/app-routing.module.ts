6e81a57b2e126ca5d5361883f08d306214244637
Levy Omolo <levy.omolo@student.moringaschool.com>
2022-01-24 18:39:42 +0300

    Add the routes links of the newly created components

--- Alumni/src/app/app-routing.module.ts
+++ Alumni/src/app/app-routing.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
+import {ChannelsComponent} from './components/channels/channels.component';
 
 import { HomePageComponent } from './components/home-page/home-page/home-page.component';
 import { BlogsComponent } from './components/blogs/blogs/blogs.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomePageComponent },
-  { path: 'blogs', component: BlogsComponent },
+  {path: '', redirectTo:'home', pathMatch:'full'},
+  {path: 'home', component: HomePageComponent },
+  {path: 'blogs', component: BlogsComponent },
+  {path: 'channels', component: ChannelsComponent },
   
-
- 
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
   {path: '', redirectTo: 'index', pathMatch: 'full'},

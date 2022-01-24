cbd0ef3a0d098bdd70289253e1dc163ef9698652
Levy Omolo <levy.omolo@student.moringaschool.com>
2022-01-24 21:35:53 +0300

    Adjust the registration component

--- Alumni/src/app/components/register/register.component.ts
+++ Alumni/src/app/components/register/register.component.ts
@@ -1,48 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
-import { AuthapiService } from 'src/app/services/authapi.service';
-
+import { UserService } from 'src/app/services/user.service';
+import { HttpClientXsrfModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [UserService]
 })
 export class RegisterComponent implements OnInit {
   passwordMismatch: boolean = false;
   usernameExists: boolean = false;
 
-  constructor(private service: AuthapiService, private router: Router) { }
+  register:any;
 
-  ngOnInit(): void {
-  }
-  registerUser(form: NgForm) {
-    this.passwordMismatch = false
-    this.usernameExists = false
-    const email = form.value.email;
-    const password = form.value.password;
-    const confirmPassword = form.value.password2;
-    const first_name = form.value.first_name;
-    const last_name = form.value.last_name;
-    const username = form.value.username;
-    if (password == confirmPassword) {
-      this.service
-        .registerUser(username, password, email, first_name, last_name)
-        .subscribe(
-          (response) => {
-            console.log(response);
-            this.router.navigateByUrl('/login');
-          },
-          (error) => {
-            this.usernameExists = true
-            console.log(error);
-          }
-        );
-    }
-    else {
-      this.passwordMismatch = true
-    }
-  }
+  constructor(private userService : UserService) {}
 
+  ngOnInit(){
+    this.register = {
+      username : '',
+      email :'',
+      password:'',
+    };
+  }
+  registerUser(){
+    this.userService.registerUser(this.register).subscribe(
+      response => {
+        alert('User ' + this.register.username + ' has been created successfully!')
+      },
+      error => console.log('error',error)
+    );
+  }
 }

6db3bdf51ebcb444aaacf274d499825800f6845c
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2022-01-24 10:49:49 +0300

    Added styling

--- Alumni/src/app/components/login/login.component.ts
+++ Alumni/src/app/components/login/login.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthapiService } from '../../services/authapi.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  showErrorMessage: boolean = false;
 
-  constructor() { }
+  constructor(private service: AuthapiService, private router: Router) {}
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  loginUser(form: NgForm) {
+    this.showErrorMessage = false;
+    const username = form.value.username;
+    const password = form.value.password;
+    this.service.loginUser(username, password).subscribe(
+      (response) => {
+        console.log(response);
+        localStorage.setItem('currentUser', JSON.stringify(response));
+        this.service.getToken(username, password).subscribe(
+          (response: any) => {
+            console.log(response);
+            localStorage.setItem('token', response.token);
+          }
+        );
+        this.router.navigateByUrl('/home');
+      },
+      (error) => {
+        console.log(error);
+        this.showErrorMessage = true;
+      }
+    );
   }
-
 }

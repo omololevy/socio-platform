006680932050e19cf9e0982d3b34cf3de252e788
Levy Omolo <levy.omolo@student.moringaschool.com>
2022-01-25 12:49:35 +0300

    Updates the routes to the newly created componenets and templates

--- Alumni/src/app/app-routing.module.ts
+++ Alumni/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
 import {ChannelsComponent} from './components/channels/channels.component';
-
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { HomePageComponent } from './components/home-page/home-page/home-page.component';
 import { BlogsComponent } from './components/blogs/blogs/blogs.component';
 
@@ -11,12 +11,13 @@ const routes: Routes = [
   {path: '', redirectTo:'home', pathMatch:'full'},
   {path: 'home', component: HomePageComponent },
   {path: 'blogs', component: BlogsComponent },
-  {path: 'channels', component: ChannelsComponent },
-  
+  {path: 'channels', component: ChannelsComponent,  data:{requiresLogin: true}},
+  {path: 'user_profile', component: UserProfileComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
   {path: '', redirectTo: 'index', pathMatch: 'full'},
   {path: '**', redirectTo: 'index', pathMatch: 'full'}
+  
 ];
 
 @NgModule({

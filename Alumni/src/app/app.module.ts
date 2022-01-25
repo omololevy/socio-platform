09c82e1b0d2a65cba042ed0bd49a6118203547bd
Levy Omolo <levy.omolo@student.moringaschool.com>
2022-01-25 13:10:53 +0300

    Update the imports and the modules with the newly added components

--- Alumni/src/app/app.module.ts
+++ Alumni/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 import { BlogsComponent } from './components/blogs/blogs/blogs.component';
 import { HomePageComponent } from './components/home-page/home-page/home-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -18,6 +20,8 @@ import { UserProfileComponent } from './components/user-profile/user-profile.com
 @NgModule({
   declarations: [
     AppComponent,
+    AboutUsComponent,
+    ContactUsComponent,
     BlogsComponent,
     HomePageComponent,
     NavbarComponent,

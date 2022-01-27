df3bcff372f82e0d809b9f481d8c32b679e6d3cb
Levy Omolo <levy.omolo@student.moringaschool.com>
2022-01-27 23:27:31 +0300

    Add the redirect logic for login and signup

--- Alumni/src/app/components/register/register.component.ts
+++ Alumni/src/app/components/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { HttpClientXsrfModule } from '@angular/common/http';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -11,11 +11,8 @@ import { HttpClientXsrfModule } from '@angular/common/http';
 export class RegisterComponent implements OnInit {
   passwordMismatch: boolean = false;
   usernameExists: boolean = false;
-
   register:any;
-
-  constructor(private userService : UserService) {}
-
+  constructor(private userService : UserService, private router: Router){}
   ngOnInit(){
     this.register = {
       username : '',
@@ -27,7 +24,8 @@ export class RegisterComponent implements OnInit {
   registerUser(){
     this.userService.registerUser(this.register).subscribe(
       response => {
-        alert('User ' + this.register.username + ' has been created successfully!')
+        console.log(response);
+            this.router.navigateByUrl('/login');
       },
       error => console.log('error',error)
     );

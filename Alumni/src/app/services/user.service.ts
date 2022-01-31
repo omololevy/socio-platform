1d14eedbdd5367ea688e1c8275931617ea8ca9a2
Levy Omolo <levy.omolo@student.moringaschool.com>
2022-01-31 13:09:55 +0300

    add the user service functions for logout, login, register, current user, and get token

--- Alumni/src/app/services/user.service.ts
+++ Alumni/src/app/services/user.service.ts
@@ -6,22 +6,51 @@ import { Observable } from 'rxjs/';
   providedIn: 'root',
 })
 export class UserService {
-  baseApiUrl = "http://127.0.0.1:8000/profile/"
+  baseAPIUrl = "http://127.0.0.1:8000"
   photoUrl = "http://res.cloudinary.com/dim8pysls/image/upload/"
 
   constructor(private http:HttpClient) { }
 
-  registerUser(userData:any):Observable<any>{
-    let headers;
-    console.log(userData)
-    headers = {headers: new HttpHeaders({
-      'Content-Type': 'application/json'
-    })
-    }
-    return this.http.post('http://127.0.0.1:8000/users/',userData, headers)
+  // registerUser(userData:any):Observable<any>{
+  //   let headers;
+  //   console.log(userData)
+  //   headers = {headers: new HttpHeaders({
+  //     'Content-Type': 'application/json'
+  //   })
+  //   }
+  //   return this.http.post('http://127.0.0.1:8000/users/',userData, headers)
+  // }
+
+  loginUser(username: string, password: string) {
+    return this.http.post(this.baseAPIUrl + '/auth/login/', 
+    {
+      username: username,
+      password: password,
+    });
   }
 
-  updateProfile(profileData:any):Observable<any>{
+  getToken(username: string, password: string) 
+  {
+    return this.http.post(this.baseAPIUrl + '/api-auth/', {
+      username: username,
+      password: password,
+    });
+  }
+
+  registerUser(username: string,password: string,email: string,first_name: string,second_name: string) 
+  {
+    return this.http.post(this.baseAPIUrl + '/users', 
+    {
+      username: username,
+      password: password,
+      email: email,
+      first_name: first_name,
+      second_name: second_name,
+    },);
+  }
+
+  updateProfile(profileData:any):Observable<any>
+  {
     let attr;
     
     attr = {headers: new HttpHeaders({
@@ -33,18 +62,19 @@ export class UserService {
 
   }
 
-  loginUser(userData:any):Observable<any>{
-    return this.http.post('http://127.0.0.1:8000/api-auth/',userData)
+  upload(file:any):Observable<any> {
+    const formData = new FormData();  
+    formData.append("file", file, file.name);
+    return this.http.post(this.photoUrl, formData)
   }
 
-  upload(file:any):Observable<any> {
-    // Create form data
-    const formData = new FormData();   
-    // Store form name as "file" with file data
-    formData.append("file", file, file.name);
-    // Make http post request over api
-    // with formData as req
-    return this.http.post(this.photoUrl, formData)
+  logout() {
+    localStorage.removeItem('currentUser');
+  }
+
+  currentUserValue() {
+    let localStorage: any = null;
+    return JSON.parse(localStorage.getItem('currentUser'));
   }
 
 

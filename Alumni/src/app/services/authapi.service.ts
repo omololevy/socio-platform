2907d9bd8fbea1fb5b9dfda4267302bee47d61d3
Levy Omolo <levy.omolo@student.moringaschool.com>
2022-01-31 13:10:28 +0300

    comment the unneccesery lines of code captured elsewhere

--- Alumni/src/app/services/authapi.service.ts
+++ Alumni/src/app/services/authapi.service.ts
@@ -8,17 +8,17 @@ export class AuthapiService {
   APIUrl = 'http://127.0.0.1:8000';
 
  constructor(private http: HttpClient) {}
-  loginUser(username: string, password: string) {
-    return this.http.post(this.APIUrl + '/app/api/auth/login/', {
-      username: username,
-      password: password,
-    });
-  }
+  // loginUser(username: string, password: string) {
+  //   return this.http.post(this.APIUrl + '/app/api/auth/login/', {
+  //     username: username,
+  //     password: password,
+  //   });
+  // }
 
-  getToken(username: string, password: string) {
-    return this.http.post(this.APIUrl + '/api-auth/', {
-      username: username,
-      password: password,
-    });
-  }
+  // getToken(username: string, password: string) {
+  //   return this.http.post(this.APIUrl + '/api-auth/', {
+  //     username: username,
+  //     password: password,
+  //   });
+  // }
 }

75cdbbea3f4036f243c574d402aafb58f0e6e702
Levy Omolo <levy.omolo@student.moringaschool.com>
2022-02-03 08:48:01 +0300

    Create a service for the fundraiser

---
+++ Alumni/src/app/services/fundraiser.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FundraiserService {
+  baseurl = "http://127.0.0.1:8000/fundraiser/"
+
+
+  constructor(private http:HttpClient) { }
+
+  fundraiser(fundraiserData:any):Observable<any>{
+    let fundraiser;
+    console.log(fundraiserData)
+    fundraiser = {headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+    }
+    return this.http.post('http://127.0.0.1:8000/fundraiser/',fundraiserData)
+  }
+  getAllFundraisers():Observable<any>{
+    return this.http.get(this.baseurl)
+  }
+}

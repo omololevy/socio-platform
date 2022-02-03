dc49d7bb5120d0393fdff096b43088afdd1531f3
Levy Omolo <levy.omolo@student.moringaschool.com>
2022-02-03 09:21:32 +0300

    Call the providers and Services for displaying the groups created

--- Alumni/src/app/components/group-posts/group-posts.component.ts
+++ Alumni/src/app/components/group-posts/group-posts.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from 'src/app/services/profile.service';
+
 
 @Component({
   selector: 'app-group-posts',
   templateUrl: './group-posts.component.html',
-  styleUrls: ['./group-posts.component.css']
+  styleUrls: ['./group-posts.component.css'],
+  providers:[ProfileService]
+
 })
 export class GroupPostsComponent implements OnInit {
+  user_id: any;
 
   constructor() { }
 

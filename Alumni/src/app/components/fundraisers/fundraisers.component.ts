c05efad362ce1f7df86335ea341453c371e48da6
Levy Omolo <levy.omolo@student.moringaschool.com>
2022-02-04 05:46:08 +0300

    Call the fundraisers from the database

--- Alumni/src/app/components/fundraisers/fundraisers.component.ts
+++ Alumni/src/app/components/fundraisers/fundraisers.component.ts
@@ -11,8 +11,9 @@ import { FundraiserService } from 'src/app/services/fundraiser.service';
   providers: [ProfileService,FundraiserService]
 })
 export class FundraisersComponent implements OnInit {
-  fundraisers = [{fund_name:''},{content:''}];
+  fundraisers = [{fund_name:''},{content:''},{start_date:''},{end_date:''},{created_by:''}];
   user_id!:any
+  fundraiser_id:any
 
   constructor( private user: ProfileService,private fundraiser: FundraiserService, private router: ActivatedRoute ) { this.getFundraisers();}
 
